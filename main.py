a052c706ed0559f8436547fd69888f4388915b0b
Harry He <asdf@asdf.com>
2019-09-27 21:06:18 -0600

    fix indentation

--- main.py
+++ main.py
@@ -3,66 +3,66 @@ from pygame.locals import *
 from enum import IntEnum
 
 class KeyMapping(IntEnum):
-  LEFT = 0
-  DOWN = 1
-  UP = 2
-  RIGHT = 3
-  TRIANGLE = 4
-  SQUARE = 5
-  X = 6
-  O = 7
-  SELECT = 8
-  START = 9
+    LEFT = 0
+    DOWN = 1
+    UP = 2
+    RIGHT = 3
+    TRIANGLE = 4
+    SQUARE = 5
+    X = 6
+    O = 7
+    SELECT = 8
+    START = 9
 
 combination_set = {KeyMapping.LEFT, KeyMapping.RIGHT, KeyMapping.UP, KeyMapping.DOWN}
 key_image = 'key.png'
 background_image = 'blank.png'
 
 def main():
-  # init
-  pygame.init()
+    # init
+    pygame.init()
 
-  # look for joysticks
-  totaljoy = pygame.joystick.get_count()
-  print(totaljoy,"joysticks total -")
+    # look for joysticks
+    totaljoy = pygame.joystick.get_count()
+    print(totaljoy,"joysticks total -")
 
-  if totaljoy < 1:
-    print('no joysticks found.')
-    sys.exit()
+    if totaljoy < 1:
+        print('no joysticks found.')
+        sys.exit()
 
-  ddrmat = pygame.joystick.Joystick(totaljoy-1)
-  ddrmat.init()
-  print ("last one is",ddrmat.get_numaxes(),"axes and",ddrmat.get_numbuttons(),"buttons")
-  
-  # set up the screen and all that stuff
-  screen = pygame.display.set_mode(flags=FULLSCREEN|HWSURFACE|DOUBLEBUF) 
-  pygame.display.set_caption('pyDDR')
-  pygame.mouse.set_visible(1)
-
-  # load images
-  key = pygame.image.load(key_image).convert()
-  background = pygame.image.load(background_image).convert()
-  
-  down_buttons = set()
-  
-  while True: 
+    ddrmat = pygame.joystick.Joystick(totaljoy-1)
+    ddrmat.init()
+    print ("last one is",ddrmat.get_numaxes(),"axes and",ddrmat.get_numbuttons(),"buttons")
     
-    event = pygame.event.poll()
-    if event.type == KEYDOWN:
-        break
+    # set up the screen and all that stuff
+    screen = pygame.display.set_mode(flags=FULLSCREEN|HWSURFACE|DOUBLEBUF) 
+    pygame.display.set_caption('pyDDR')
+    pygame.mouse.set_visible(1)
 
-    if event.type == JOYBUTTONDOWN:
-      down_buttons.add(event.button)
-      if combination_set == down_buttons:
-        screen.blit(key, (0, 0))
-        pygame.display.flip()
+    # load images
+    key = pygame.image.load(key_image).convert()
+    background = pygame.image.load(background_image).convert()
+    
+    down_buttons = set()
+    
+    while True: 
         
-    if event.type == JOYBUTTONUP:
-      down_buttons.remove(event.button)
-      if combination_set != down_buttons:
-        screen.blit(background, (0, 0))
-        pygame.display.flip()
+        event = pygame.event.poll()
+        if event.type == KEYDOWN:
+            break
+
+        if event.type == JOYBUTTONDOWN:
+            down_buttons.add(event.button)
+            if combination_set == down_buttons:
+                screen.blit(key, (0, 0))
+                pygame.display.flip()
+            
+        if event.type == JOYBUTTONUP:
+            down_buttons.remove(event.button)
+            if combination_set != down_buttons:
+                screen.blit(background, (0, 0))
+                pygame.display.flip()
 
     
 if __name__ == '__main__': 
-  main()
+    main()
